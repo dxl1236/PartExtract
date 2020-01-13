8b5593ff7230056a59be49fb40837779accd0844
dxl1236 <31926240+dxl1236@users.noreply.github.com>
2020-01-13 19:24:51 +0800

    Create 安装.bat

---
+++ bat/安装.bat
@@ -0,0 +1,15 @@
+@echo off
+Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
+Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行！&&Pause >nul&&Exit)
+Rd "%WinDir%\System32\test_permissions" 2>NUL
+if EXIST "%UGII_ROOT_DIR%" (
+xcopy /h /y /q "%~dp07z.exe" "%UGII_ROOT_DIR%"
+xcopy /h /y /q "%~dp0PartExtract.bat"  "%UGII_ROOT_DIR%"
+reg delete "HKEY_CLASSES_ROOT\*\shell\PartExtract" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\PartExtract"   /d "NX装配提取打包" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\PartExtract\command"   /d "%UGII_ROOT_DIR%\PartExtract.bat  \"%%1\""  /f
+echo.安装完成。
+ping 127.0.0.1 -n 3 >nul 
+) else echo.环境变量设置有问题，请手动复制所以文件到UGII目录后运行注册右键.bat！&&pause>nul&&exit 
+
+
