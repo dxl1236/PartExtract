0aace755b5e1ffaa8fad4bb0196a3909a008d190
dxl1236 <31926240+dxl1236@users.noreply.github.com>
2020-01-16 20:49:36 +0800

    Update 安装.bat

--- bat/安装.bat
+++ bat/安装.bat
@@ -5,9 +5,9 @@ Rd "%WinDir%\System32\test_permissions" 2>NUL
 if EXIST "%UGII_ROOT_DIR%" (
 xcopy /h /y /q "%~dp07z.exe" "%UGII_ROOT_DIR%"
 xcopy /h /y /q "%~dp0PartExtract.bat"  "%UGII_ROOT_DIR%"
-reg delete "HKEY_CLASSES_ROOT\*\shell\PartExtract" /f
-reg add "HKEY_CLASSES_ROOT\*\shell\PartExtract"   /d "NX装配提取打包" /f
-reg add "HKEY_CLASSES_ROOT\*\shell\PartExtract\command"   /d "%UGII_ROOT_DIR%\PartExtract.bat  \"%%1\""  /f
+reg delete "HKEY_CLASSES_ROOT\*\shell\PartExtract-bat" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\PartExtract-bat"   /d "NX装配树提取打包(bat)" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\PartExtract-bat\command"   /d "%UGII_ROOT_DIR%\PartExtract.bat  \"%%1\""  /f
 echo.安装完成。
 ping 127.0.0.1 -n 3 >nul 
 ) else echo.环境变量设置有问题，请手动复制所以文件到UGII目录后运行注册右键.bat！&&pause>nul&&exit 
