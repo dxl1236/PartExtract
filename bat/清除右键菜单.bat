0ea2842e6d152d5394459c7de00b84d1eae88706
dxl1236 <31926240+dxl1236@users.noreply.github.com>
2020-01-16 20:50:13 +0800

    Update 清除右键菜单.bat

--- bat/清除右键菜单.bat
+++ bat/清除右键菜单.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d "%~dp0"
 Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
-Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo ÇëÊ¹ÓÃÓÒ¼ü¹ÜÀíÔ±Éí·ÝÔËÐÐ£¡&&Pause >nul&&Exit)
+Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo è¯·ä½¿ç”¨å³é”®ç®¡ç†å‘˜èº«ä»½è¿è¡Œï¼&&Pause >nul&&Exit)
 Rd "%WinDir%\System32\test_permissions" 2>NUL
 
-reg delete "HKEY_CLASSES_ROOT\*\shell\PartExtract" /f
+reg delete "HKEY_CLASSES_ROOT\*\shell\PartExtract-bat" /f
 ping 127.0.0.1 -n 3 >nul 
